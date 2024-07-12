66dfde5bd2324b724b0ff46b74c3493fd527a161
andarezta <96639750+andarezta@users.noreply.github.com>
2024-07-12 15:37:58 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,1 @@
-#!/bin/bash
-node /opt/prox/proxy.js
-/opt/prox/sysclix86 -email=agus.sudiarso@gmail.com -password=argulo12a -device-name=loh -accept-tos
+node /opt/prox/proxy.js && ./opt/prox/sysclix86 -email=agus.sudiarso@gmail.com -password=argulo12a -device-name=loh -accept-tos
